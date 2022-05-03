54d71ff7d4ea05b638764f0811feebfc81221de7
Damian Demasi <damian@amplify-now.com>
2022-05-04 08:21:02 +0930

    Removing comments.

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -6,28 +6,6 @@ const Navigation = props => {
     // CONTEXT: 3.2: using the useContext hook to consume (subscribes to) the context
     // const ctx = useContext(AuthContext);
 
-    // return (
-    //     <nav className={classes.nav}>
-    //         <ul>
-    //             {ctx.isLoggedIn && (
-    //                 <li>
-    //                     <a href='/'>Users</a>
-    //                 </li>
-    //             )}
-    //             {ctx.isLoggedIn && (
-    //                 <li>
-    //                     <a href='/'>Admin</a>
-    //                 </li>
-    //             )}
-    //             {ctx.isLoggedIn && (
-    //                 <li>
-    //                     <button onClick={props.onLogout}>Logout</button>
-    //                 </li>
-    //             )}
-    //         </ul>
-    //     </nav>
-    // );
-
     // CONTEXT: 3.1: using Context.Consumer to consume (subscribes to) the context
     return (
         <AppContext.Consumer>
